cd533f966736500d0351e1b685aea6c3500746dc
Shreya Kuchibhatla <kuchibhatla.shreya@gmail.com>
2023-12-11 13:27:51 -0500

    Added docstrings

--- src/main/reddit_grpc/server/server.py
+++ src/main/reddit_grpc/server/server.py
@@ -5,7 +5,15 @@ import reddit_pb2
 import reddit_pb2_grpc
 
 class RedditService(reddit_pb2_grpc.RedditServiceServicer):
+    """
+    Implements the RedditService gRPC service, providing functionalities
+    similar to a simplified version of Reddit.
+    """
     def __init__(self):
+        """
+        Implements the RedditService gRPC service, providing functionalities
+        similar to a simplified version of Reddit.
+        """
         self.posts = {}
         self.comments = {}
         self.posts_and_comments = {}
@@ -15,7 +23,10 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         self.setup_data()
 
     def setup_data(self):
-
+        """
+        Initializes the RedditService with empty data structures for posts,
+        comments, and related entities.
+        """
         for post_id, post in self.posts.items():
             self.posts_and_comments[post_id] = post.score
         for comment_id, comment in self.comments.items():
@@ -46,20 +57,45 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
                     comment.reply_ids.append(nested_comment_id)
 
     def calculate_new_score(self, item_id):
+        """
+        Calculates and updates the new score for a post or comment.
+        Args:
+            item_id: ID of the post or comment.
+        Returns:
+            The new score after incrementing.
+        """
         self.posts_and_comments[item_id] += 1
         return self.posts_and_comments[item_id]
 
     def get_next_post_id(self):
+        """
+        Generates the next post ID.
+        Returns:
+            A string representing the next post ID.
+        """
         post_id = f'post_{self.next_post_id}'
         self.next_post_id += 1
         return post_id
 
     def get_next_comment_id(self):
+        """
+        Generates the next comment ID.
+        Returns:
+            A string representing the next comment ID.
+        """
         comment_id = f'comment_{self.next_comment_id}'
         self.next_comment_id += 1
         return comment_id
 
     def CreatePost(self, request, context):
+        """
+        Creates a new post with given details.
+        Args:
+            request: An instance of CreatePostRequest containing the post details.
+            context: gRPC context.
+        Returns:
+            A CreatePostResponse containing the newly created post.
+        """
         post_id = self.get_next_post_id()
         post = request.post
         post.id = post_id
@@ -68,6 +104,14 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         return reddit_pb2.CreatePostResponse(post=post)
 
     def VotePost(self, request, context):
+        """
+        Handles voting (upvote/downvote) for a post.
+        Args:
+            request: An instance of VotePostRequest containing the post ID and vote type.
+            context: gRPC context.
+        Returns:
+            A VotePostResponse containing the new score of the post.
+        """
         post = self.posts.get(request.post_id)
         if not post:
             context.abort(grpc.StatusCode.NOT_FOUND, 'Post not found')
@@ -76,13 +120,28 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         return reddit_pb2.VotePostResponse(new_score=post.score)
 
     def RetrievePost(self, request, context):
+        """
+        Retrieves a post based on the provided post ID.
+        Args:
+            request: An instance of RetrievePostRequest containing the post ID.
+            context: gRPC context.
+        Returns:
+            A RetrievePostResponse containing the post details.
+        """
         post = self.posts.get(request.post_id)
         if not post:
             context.abort(grpc.StatusCode.NOT_FOUND, 'Post not found')
         return reddit_pb2.RetrievePostResponse(post=post)
 
     def CreateComment(self, request, context):
-
+        """
+        Creates a new comment for a post or another comment.
+        Args:
+            request: An instance of CreateCommentRequest containing the comment details.
+            context: gRPC context.
+        Returns:
+            A CreateCommentResponse containing the newly created comment.
+        """
         try:
             comment_id = self.get_next_comment_id()
             comment = request.comment
@@ -107,6 +166,14 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         return reddit_pb2.CreateCommentResponse(comment=comment)
 
     def VoteComment(self, request, context):
+        """
+        Handles voting (upvote/downvote) for a comment.
+        Args:
+            request: An instance of VoteCommentRequest containing the comment ID and vote type.
+            context: gRPC context.
+        Returns:
+            A VoteCommentResponse containing the new score of the comment.
+        """
         comment = self.comments.get(request.comment_id)
         if not comment:
             context.abort(grpc.StatusCode.NOT_FOUND, 'Comment not found')
@@ -115,6 +182,14 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         return reddit_pb2.VoteCommentResponse(new_score=comment.score)
 
     def RetrieveTopComments(self, request, context):
+        """
+        Retrieves the top comments of a post.
+        Args:
+            request: An instance of RetrieveTopCommentsRequest containing the post ID and number of comments to retrieve.
+            context: gRPC context.
+        Returns:
+            A RetrieveTopCommentsResponse containing the top comments of the post.
+        """
         # Find the post and its associated comments
         post = self.posts.get(request.post_id)
         if not post:
@@ -132,6 +207,14 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
 
 
     def ExpandCommentBranch(self, request, context):
+        """
+        Expands a comment branch to retrieve its replies.
+        Args:
+            request: An instance of ExpandCommentBranchRequest containing the comment ID and number of replies to retrieve.
+            context: gRPC context.
+        Returns:
+            An ExpandCommentBranchResponse containing the comment and its replies.
+        """
         main_comment = self.comments.get(request.comment_id)
         if not main_comment:
             context.abort(grpc.StatusCode.NOT_FOUND, 'Comment not found')
@@ -140,6 +223,14 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
         return reddit_pb2.ExpandCommentBranchResponse(comments=[main_comment] + replies)
 
     def MonitorUpdates(self, request_iterator, context):
+        """
+        Monitors updates to posts and comments and streams the updated scores.
+        Args:
+            request_iterator: An iterator over MonitorUpdatesRequest objects.
+            context: gRPC context.
+        Yields:
+            MonitorUpdatesResponse objects containing updated scores for monitored items.
+        """
         for request in request_iterator:
             item_id = request.post_id or request.comment_id
             print(f"Monitoring request received for: {item_id}")  # Debugging line
@@ -151,6 +242,12 @@ class RedditService(reddit_pb2_grpc.RedditServiceServicer):
                 context.abort(grpc.StatusCode.NOT_FOUND, f'{item_id} not found')
 
 def serve(host, port):
+    """
+    Starts the gRPC server with the RedditService.
+    Args:
+        host: The hostname to listen on.
+        port: The port number to listen on.
+    """
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     reddit_pb2_grpc.add_RedditServiceServicer_to_server(RedditService(), server)
     server.add_insecure_port(f'{host}:{port}')
@@ -162,7 +259,7 @@ def serve(host, port):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Reddit gRPC Server')
     parser.add_argument('--host', type=str, default='localhost', help='Host to serve on')
-    parser.add_argument('--port', type=int, default=50559, help='Port to serve on')
+    parser.add_argument('--port', type=int, default=5555, help='Port to serve on')
     args = parser.parse_args()
 
     serve(args.host, args.port)
